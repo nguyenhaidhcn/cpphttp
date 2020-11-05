2fd70287859d7937b575505a1358c6fc14fd92e8
haint <haint@HAINTC1D2>
2020-11-05 16:39:37 +0800

    init

---
+++ main.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <memory>
+#include "http_server.h"
+#include <map>
+
+mg_serve_http_opts HttpServer::s_server_option;
+std::string HttpServer::s_web_dir = "./web";
+std::map<std::string, ReqHandler> HttpServer::s_handler_map;
+std::set<mg_connection *> HttpServer::s_websocket_session_set;
+
+bool handle_fun1(std::string url, std::string body, mg_connection *c, OnRspCallback rsp_callback)
+{
+	// do sth
+	std::cout << "handle fun1" << std::endl;
+	std::cout << "url: " << url << std::endl;
+	std::cout << "body: " << body << std::endl;
+
+	rsp_callback(c, "rsp1");
+
+	return true;
+}
+
+bool handle_fun2(std::string url, std::string body, mg_connection *c, OnRspCallback rsp_callback)
+{
+	// do sth
+	std::cout << "handle fun2" << std::endl;
+	std::cout << "url: " << url << std::endl;
+	std::cout << "body: " << body << std::endl;
+
+	rsp_callback(c, "{sfdw:wfsf}");
+
+	return true;
+}
+
+int main(int argc, char *argv[]) 
+{
+	std::string port = "7999";
+	auto http_server = std::shared_ptr<HttpServer>(new HttpServer);
+	http_server->Init(port);
+	// add handler
+	http_server->AddHandler("/api/fun1", handle_fun1);
+	http_server->AddHandler("/api/fun2", handle_fun2);
+
+	http_server->Start();
+	
+
+	return 0;
+}
